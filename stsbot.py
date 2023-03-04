e88e79148207d4a2d47aaa0ce6b5d80e9f7b5203
panjandemi <vkonynenburg@googlemail.com>
2023-03-05 10:17:53 +1100

    Added a few bits of functionality - If the bot fails to find a wiki page,     it will delete the link and apologise
    - Input to the bot is now case-insensitive :relieved:
        Managed to get that all sorted so I'm pretty happy.
    
    I'll probably keep working on this at some point but it's at a state
    where i'm pretty happy with it. Happy Slaying!

--- stsbot.py
+++ stsbot.py
@@ -1,6 +1,12 @@
 import discord
 import re
 import os
+import time
+
+## <    Variables   >
+deleteTime = 3
+embedTime = 3
+##
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,10 +15,11 @@ print(f'Loaded Token')
 #restricting access to the token. Just make ur own .env lol
 
 # This bot is pretty normal. it just grabs words wrapped in [[]] and posts the wiki link to the page
-# Written by Panjandemi# on discord - Feel free to reach out if you've got questions
+# Written by Panjandemi#9301 on discord - Feel free to reach out if you've got questions
 # dated 15/2/23
 
 intents = discord.Intents.default()
+intents.messages = True
 intents.message_content = True #Defining bot intents. Bot can't connect to discord if it doesn't have the right intents, so this needs to be done.
 print(f'Loaded Intents')
 
@@ -23,17 +30,23 @@ async def on_ready():
     print (f'Logged in as {client.user} - Get Slaying!')
 #When the bot logs on to discord, it sends this message into the shell.
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return  # Prevents message loops in the off case that the bot would send a message that it could respond to
+        apology = "Sorry, I couldn't find a wiki page with that name"
+        time.sleep(embedTime)
+        if ((not(message.embeds)) & (message.content != apology)):
+            print(f'Failed to embed') 
+            await message.delete() #If the message does not embed, delete it
+            await message.channel.send(apology)
+        elif (message.content == apology):
+            time.sleep(deleteTime)
+            await message.delete()
+        else:
+            return  # Prevents message loops in the off case that the bot would send a message that it could respond to
     
     message_content = message.content #gets the message content and puts it into a variable to be checked with the regex for any instances of the bot being called
-
-    #if discord.TextChannel.name == "bots":
-    #putting this here to restrict it to a particular channel channel names and whatnot
-    #i'd love to figure out how to get channel restrictions going. doesn't seem to want to work
-
 
     names = re.findall("\[\[[^\[\]]*\]\]",message_content) # Yes I know this sucks so bad to read but it's really simple I promise.
     # It's a regular expression that checks for any instance of words surrounded by [[]], *except* for when those brackets contain other brackets.
@@ -42,13 +55,46 @@ async def on_message(message):
     wiki = "https://slay-the-spire.fandom.com/wiki/" # Just a quick definition for the wiki link.
     # I put this here because I thought the wiki had seperate places for cards and relics, but it turns out that's not the case
     # Still good to have for modularity's sake, just swap out the wiki page for a different wiki and hey presto, there's your wiki bot sorted. 
+    # Provided of course that wiki uses the same catagorisation scheme as the STS wiki,,, Look whatever. It's so fine dw about it.
     
     for x in names:
-        namestr = ''.join(x) #converting the list from the findall into a string.
-    
-        namestr = re.sub("\s","_",namestr) #replacing white space with underscores to ensure that it interacts with hyperlinks correctly.
 
+        ### <    Manipulating String     >
+
+        nonCapWords = ['for','and','of','the','to','with']  #I went through the game and found all instances of words that aren't capitalised.
+
+        ##  <   Converting to a lowercase String    >
+        namestr = ''.join(x) #converting the list from the findall into a string.
         namestr = re.sub("[\[\]]",'',namestr) #trim brackets off the string.
+        namestr = namestr.lower() #reduces the string to lowercase
+        ##
+
+        ##  <   Checking for non-capitalised words  >
+        namestrList = re.split("\s", namestr)
+        A = 0
+        for a in namestrList:
+            
+            noCap = False
+            for b in nonCapWords:
+                if a == b:
+                    noCap = True
+        ##  <   Capitalising Words  >
+            if noCap == False:          
+                workedWord = str(a)
+                workedWord = list(workedWord)
+                workedWord[0] = workedWord[0].swapcase()
+                workedWord = ''.join(workedWord)
+                #Wow I hope this works
+            else:
+                workedWord = str(a)
+            
+            namestrList[(A)] = workedWord
+            A += 1     
+        ##
+        ##   
+
+        namestr = '_'.join(namestrList) #Joining the string back together with underscores
+        ###
 
         await message.channel.send(wiki + namestr) #Posts the wiki link into the chat
         print(f'{message.author} called for {wiki+namestr}') #sends a message into the shell to say that it posted a wiki link into the chat
